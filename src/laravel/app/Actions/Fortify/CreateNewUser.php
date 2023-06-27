d291736f68ea17c449c1646106f4ddfdb80dbc46
yaharu <genga711@gmail.com>
2023-06-28 08:44:47 +0900

    add fortify

---
+++ src/laravel/app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use App\Services\CreateNewUserServiceInterface;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    private readonly CreateNewUserServiceInterface $createNewUserService;
+
+    public function __construct(CreateNewUserServiceInterface $createNewUserService)
+    {
+        $this->createNewUserService = $createNewUserService;
+    }
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        try{
+            return $this->createNewUserService->run($input);
+        }catch (\InvalidArgumentException $e){
+            error_log($e->getMessage());
+            abort(404);
+        }
+    }
+}
